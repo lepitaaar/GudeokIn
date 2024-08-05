71610e412f9a8c6c51ff27eccf5d58f9a6b5176f
Lepitar <66681282+mojobsrc@users.noreply.github.com>
2024-08-06 08:56:35 +0900

    Update firebase.ts

--- src/app/lib/firebase.ts
+++ src/app/lib/firebase.ts
@@ -4,7 +4,7 @@ import { getMessaging, getToken, isSupported } from "firebase/messaging";
 const firebaseConfig = {
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
+    projectId: "gudeokin-e4c75",
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.FIREBASE_APP_ID,
